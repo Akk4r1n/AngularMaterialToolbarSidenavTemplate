6b37e4a969b27008aa7da82264eb809cc0b30f2a
akk4r1n <davidriewe01@gmail.com>
2021-03-07 00:33:46 +0100

    add basic functionality for sidenav

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,4 +1,6 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
+import { MatDrawerMode } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-sidenav',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavComponent implements OnInit {
 
-  constructor() { }
+  sidenavOpened : boolean = true;
+  sidenavMode: MatDrawerMode = 'side';
+  sidenavWidth : string = "25%";
+
+  constructor(breakpointObserver : BreakpointObserver) {
+    breakpointObserver.observe([
+      Breakpoints.HandsetLandscape,
+      Breakpoints.HandsetPortrait
+    ]).subscribe(result => {
+      console.log(result);
+      if (result.matches) {
+        this.activateHandsetLayout();
+      }
+    })
+
+    breakpointObserver.observe([
+      Breakpoints.TabletLandscape,
+      Breakpoints.TabletPortrait
+    ]).subscribe(result => {
+      if (result.matches) {
+        this.activateTabletLayout();
+      }
+    })
+  }
 
   ngOnInit(): void {
+
+  }
+
+  activateTabletLayout(): void {
+    this.sidenavMode = "side";
+    this.sidenavWidth = "25%";
+    this.sidenavOpened = false;
+  }
+
+  activateHandsetLayout(): void {
+    this.sidenavMode = 'over';
+    this.sidenavWidth = "75%";
+    this.sidenavOpened = false;
+  }
+
+  toggleDrawer(): void {
+    this.sidenavOpened = !this.sidenavOpened;
   }
 
 }
